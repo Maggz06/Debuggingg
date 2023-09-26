b382217b4f5dca5efb7b472efd45c108d265f036
Maggz06 <marcus.grundevik@gmail.com>
2023-09-26 16:07:05 +0200

    bkub

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, player.transform.position.z - 10);
+    }
+}
--- Assets/Scripts/NewScript.cs
+++ Assets/Scripts/NewScript.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class NewScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start  ()
     {
-        
+        Debug.Log("this project is a complete mess");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject camera = GameObject.FindWithTag("Player");
-        transform.position = camera.transform.position + new Vector3(0,0,-10);
+
+
+
+
     }
 }
